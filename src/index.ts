ce22ea464023ba43727e027a0f751bb6b169e9b1
Sayed Abd-Ellah <sa2098852@gmail.com>
2022-11-20 16:33:33 +0200

    latest

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express, { Application, Request, Response } from 'express'
+import express, { Request, Response } from 'express'
 import morgan from 'morgan'
 import * as dotenv from 'dotenv'
 
@@ -6,17 +6,9 @@ dotenv.config()
 
 const PORT = process.env.PORT || 3000
 // create an instance server
-const app: Application = express()
+const app = express()
 // HTTP request logger middleware
 app.use(morgan('short'))
-
-// add routing for / path
-app.get('/', (req: Request, res: Response) => {
-  res.json({
-    message: 'Hello World 🌍'
-  })
-})
-
 // start express server
 app.listen(PORT, () => {
   console.log(`Server is starting at prot:${PORT}`)
